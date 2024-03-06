e3205f20f4fab42d8487945e2961572394b9e7d7
Kaillr <kaillerplayz@gmail.com>
2024-03-06 15:53:09 +0100

    Continued makin the website
    Co-Authored-By: wenomechainsama1 <156892696+wenomechainsama1@users.noreply.github.com>
    Co-Authored-By: Akaneyana <157577591+Akaneyana@users.noreply.github.com>

--- js/scriptTest.js
+++ js/scriptTest.js
@@ -1,5 +1,7 @@
 var passwordInput = document.getElementById('passwordInput');
 var toggleButton = document.getElementById('toggleButton');
+var getDataButton = document.getElementById('getDataButton');
+var usernameInfo = document.getElementById('usernameInfo');
 
 passwordInput.addEventListener('focus', function () {
     toggleButton.textContent = 'Hide';
@@ -19,3 +21,39 @@ toggleButton.addEventListener('click', function (event) {
         toggleButton.textContent = 'Show';
     }
 });
+
+getDataButton.addEventListener('click', function () {
+    event.preventDefault();
+    var username = usernameInfo.value;
+    var password = passwordInput.value;
+
+    if (username === '' || password === '') {
+        alert('Please fill in both fields');
+    } else if (username === 'Fucmass' && password === 'suckonmadick') {
+        window.location = "browse.html";
+    }
+    else if (username === 'Kailler' && password === 'morendin') {
+        window.location = "browse.html";
+    }
+    else if (username === 'Akane' && password === 'zestyzaza') {
+        window.location = "browse.html";
+    }
+    else if (username === 'shitosuplayers@gmail.com' && password === 'ferozzogsara') {
+        window.location = "browse.html";
+    }
+    else {
+        alert('Incorrect username or password');
+    }
+
+
+});
+
+document.getElementById("LearnMore").addEventListener("click", function () {
+    displayElement.style.display = "block";
+    displayElement.textContent = "The information collected by Google reCAPTCHA is subject to the Google Privacy Policy and Terms of Service, and is used for providing, maintaining and improving the reCAPTCHA service and for general security purposes (it is not used for personalised advertising by Google).";
+});
+
+document.getElementById("LearnMore").addEventListener("click", function() {
+    var learnMoreButton = document.getElementById("LearnMore");
+    learnMoreButton.style.display = "none";
+  });
